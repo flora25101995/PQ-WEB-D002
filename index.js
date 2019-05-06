e3df23912b151585aaabe078c035b2a91e0a09d1
flora25101995 <flora25101995@gmail.com>
2019-05-06 21:09:35 +0700

    'update01'

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hollo World!')
+let pg = require('pg')
+let client = new pg.Client(
+    {
+        user: 'postgres',
+        password: 'quyen1995',
+        database: 'csv',
+        host: 'localhost',
+        port: 5432
+    }
+)
+
+client.connect()
+
+// client.query('select * from dm_nv', function(err, results) 
+// {
+//     console.log(err, results.rows)
+// })
+
+//Copy tu https://expressjs.com/en/starter/hello-world.html
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => 
+{
+    client.query('select * from dm_nv', function(err, results) 
+    {
+        res.send(JSON.stringify(results.rows))
+        client.end()
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
